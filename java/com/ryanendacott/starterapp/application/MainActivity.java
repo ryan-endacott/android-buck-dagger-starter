4639fbd1aaab04f1cbf083344cbafbc96f1d46ae
Ryan Endacott <rzeg24@gmail.com>
2014-12-02 15:20:25 -0600

    Renamed activity to mainactivity.

--- java/com/ryanendacott/starterapp/application/MainActivity.java
+++ java/com/ryanendacott/starterapp/application/MainActivity.java
@@ -3,7 +3,7 @@ package com.ryanendacott.starterapp.application;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MyFirstActivity extends Activity {
+public class MainActivity extends Activity {
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
